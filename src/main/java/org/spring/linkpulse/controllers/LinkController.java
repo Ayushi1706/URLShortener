192db7b01a746ce077481f31b9aeae94ab4d4995
Ayushi1706 <ayushi1705singh@gmail.com>
2026-09-05 19:40:00 +0530

    Add LinkController and LinkService

--- src/main/java/org/spring/linkpulse/controllers/LinkController.java
+++ src/main/java/org/spring/linkpulse/controllers/LinkController.java
@@ -1,4 +1,23 @@
 package org.spring.linkpulse.controllers;
 
+import org.spring.linkpulse.dto.CreateLinkRequest;
+import org.spring.linkpulse.dto.LinkResponse;
+import org.spring.linkpulse.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/links")
+@CrossOrigin
 public class LinkController {
+    @Autowired
+    private LinkService linkService;
+    @PostMapping("")
+    public ResponseEntity<LinkResponse> createLink(
+            @RequestBody CreateLinkRequest request
+    ) {
+        LinkResponse linkResponse = linkService.createLink(request);
+        return ResponseEntity.ok(linkResponse);
+    }
 }
--- src/main/java/org/spring/linkpulse/services/LinkService.java
+++ src/main/java/org/spring/linkpulse/services/LinkService.java
@@ -1,7 +1,27 @@
 package org.spring.linkpulse.services;
 
+import org.spring.linkpulse.dto.CreateLinkRequest;
+import org.spring.linkpulse.dto.LinkResponse;
+import org.spring.linkpulse.models.Link;
+import org.spring.linkpulse.repository.LinkRepository;
+import org.spring.linkpulse.util.Base62Encoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LinkService {
+    @Autowired
+    private LinkRepository linkRepository;
+    @Value("${app.base-url}")
+    private String baseUrl;
+    public LinkResponse createLink(CreateLinkRequest request) {
+        Link link = new Link();
+        link.setOriginalUrl(request.url());
+        link = linkRepository.save(link);
+        link.setShortCode(Base62Encoder.encode(link.getId()));
+        linkRepository.save(link);
+        String shortUrl = baseUrl + "/" + link.getShortCode();
+        return new LinkResponse(shortUrl);
+    }
 }
