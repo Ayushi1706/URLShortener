29669f5ed32fd4eb5b0624e142dad09860e6c9b8
Ayushi1706 <ayushi1705singh@gmail.com>
2026-09-06 19:40:09 +0530

    Add findByShortCode method to LinkRepository

--- src/main/java/org/spring/linkpulse/repository/LinkRepository.java
+++ src/main/java/org/spring/linkpulse/repository/LinkRepository.java
@@ -4,6 +4,9 @@ import org.spring.linkpulse.models.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {
+    Optional<Link> findByShortCode(String shortCode);
 }
