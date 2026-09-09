e8a51d2867acef6292d723296895c6bf593b1b19
Ayushi1706 <ayushi1705singh@gmail.com>
2026-09-09 19:11:18 +0530

    Add rate limiting to link creation and configure Bucket4j

---
+++ src/main/java/org/spring/linkpulse/config/RateLimiterConfig.java
@@ -0,0 +1,25 @@
+package org.spring.linkpulse.config;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RateLimiterConfig {
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String ipAddress) {
+        return buckets.computeIfAbsent(ipAddress, key -> newBucket());
+    }
+
+    private Bucket newBucket() {
+        Bandwidth limit = Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1)));
+        return Bucket.builder().addLimit(limit).build();
+    }
+}
--- src/main/java/org/spring/linkpulse/controllers/LinkController.java
+++ src/main/java/org/spring/linkpulse/controllers/LinkController.java
@@ -1,9 +1,13 @@
 package org.spring.linkpulse.controllers;
 
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
+import org.spring.linkpulse.config.RateLimiterConfig;
 import org.spring.linkpulse.dto.CreateLinkRequest;
 import org.spring.linkpulse.dto.LinkResponse;
 import org.spring.linkpulse.services.LinkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +17,22 @@ import org.springframework.web.bind.annotation.*;
 public class LinkController {
     @Autowired
     private LinkService linkService;
+    @Autowired
+    private RateLimiterConfig rateLimiterConfig;
+
     @PostMapping("")
-    public ResponseEntity<LinkResponse> createLink(
-            @RequestBody CreateLinkRequest request
+    public ResponseEntity<?> createLink(
+            @RequestBody CreateLinkRequest request,
+            HttpServletRequest httpRequest
     ) {
-        LinkResponse linkResponse = linkService.createLink(request);
-        return ResponseEntity.ok(linkResponse);
+        String ip = httpRequest.getRemoteAddr();
+        Bucket bucket = rateLimiterConfig.resolveBucket(ip);
+        if (bucket.tryConsume(1)) {
+            LinkResponse linkResponse = linkService.createLink(request);
+            return ResponseEntity.ok(linkResponse);
+        } else{
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Rate limit exceeded. Try again in a minute.");
+        }
     }
 }
